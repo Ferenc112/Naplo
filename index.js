c22413a477e33e542a5719c96f5335cd8c0cb722
Ferenc112 <ferenc06szabo@gmail.com>
2026-02-11 13:28:07 +0100

    alapprogram

---
+++ index.js
@@ -0,0 +1,34 @@
+const diakok = [
+  { nev: "Anna", tantargy: "Matematika", jegy: 5 },
+  { nev: "Anna", tantargy: "Magyar", jegy: 4 },
+  { nev: "Bence", tantargy: "Matematika", jegy: 3 },
+  { nev: "Bence", tantargy: "Angol", jegy: 5 },
+  { nev: "Csilla", tantargy: "Informatika", jegy: 5 },
+  { nev: "Csilla", tantargy: "Matematika", jegy: 4 }
+];
+
+//1 minden diákj jegy növelése 1-el    ma p + spreaf operátor használata
+
+
+
+//2 szürés akinek jegye 5      filter
+
+
+
+//3 Öszes jegy átlag kiszámitása    reduce fügvény
+
+
+
+//4 Keresd meg a ez 1 diákot aki infobol 5 kapott      find
+
+
+
+//5 van e olyan diák aki 1 jegye van     some
+
+
+
+//6 minden jegy legaláb 3        every
+
+
+
+//7 uj diák hozá adás       spread operátor
